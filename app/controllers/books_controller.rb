c921cd64ffc3518a1ac4f655fa2cca62f31206ae
Mako9675 <Mako9675@users.noreply.github.com>
2023-01-29 08:46:14 +0000

    [add] ヘッダーのアイコン表示/ログインあり時のヘッダーメニュー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -13,10 +14,10 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -27,21 +28,21 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
   def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+    book = Book.find(params[:id])
+    book.destoy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/top'
-  get 'homes/about' => "homes#about"
+  get 'home/about' => "home#about", as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
   
