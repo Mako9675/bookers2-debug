749597fd9482092ef95ec60ac7188891277ab775
Mako9675 <Mako9675@users.noreply.github.com>
2023-02-04 07:22:59 +0000

    [add]フォロー/解除機能実装,ユーザー一覧のビュー整え

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,25 +1,14 @@
 class RelationshipsController < ApplicationController
   def create
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
-  end
-  def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
-  end
-
-  def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
+    redirect_back(fallback_location: root_path)
   end
   
-  private
-  def follow_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   
   has_many :relationships, class_name: "Relationship", foreign_key: "follow_id", dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :follower
-  has_many :followers, through: :reverse_of_relationships, source: :follow
+  has_many :followers, through: :reverse_relationships, source: :follow
   
   
   validates :name, length: { minimum:2, maximum: 20 }, uniqueness: true
@@ -29,16 +29,16 @@ class User < ApplicationRecord
   end
   
   
-  def follow(user_id)
-    relationships.create(follower_id: user_id)
+  def follow(user)
+    relationships.create(follower_id: user.id)
   end
   
-  def unfollow(user_id)
-    relationships.find_by(follower_id: user_id).destroy
+  def unfollow(user)
+    relationships.find_by(follower_id: user.id).destroy
   end
   
-  def following?(user_id)
-    followings.include?(@user)
+  def following?(user)
+    followings.include?(user)
   end
   
 end
